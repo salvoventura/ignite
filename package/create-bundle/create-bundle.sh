98ed5a73176cb9b447e796db3258299d7466be3e
salvoventura <venturasalvatore@yahoo.it>
2015-10-14 13:46:40 -0700

    Adding package scripts
    Script to create a shell bundle file that handles the whole installation
    on a fresh Ubuntu 15 machine.
    
    You have to run the "create-bundle.sh" somewhere to generate the
    "ignite-bundle.sh" file, which is the installer script inclusive of the
    code part of this app.
    
    Once you have the "ignite-bundle.sh", you can distribute that, and it
    has been tested to work on Ubuntu 15 server.

---
+++ package/create-bundle/create-bundle.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+################################################################################
+#  Author: Salvatore Ventura
+# Purpose: Create the bundle installer from the github zip file
+#    Date: 14 Oct 2015
+################################################################################
+if [ "`whoami`" != "root"  ]; then
+   echo "You must run this script as root"
+   echo "Example: sudo install.sh"
+   exit 1
+fi
+
+#
+# Constants
+#
+IGNITE_ZIP_FILE_URL=https://github.com/salvoventura/ignite/archive/master.zip
+SCRIPT_TEMPLATE=templates/ignite.sh.tpl
+INSTALLER=ignite-bundle.sh
+CURDIR=`pwd`
+TMPDIR=`mktemp -d`
+
+# Initial check/update
+#apt-get update
+#apt-get -y install unzip
+
+# Get the file, unzip, correct the folder name and tar/bzip2 it
+cd ${TMPDIR}
+wget ${IGNITE_ZIP_FILE_URL} -O master.zip
+unzip master.zip
+mv ignite-master/ ignite/
+tar cf ignite.tar ignite/
+bzip2 -9 ignite.tar
+
+# Create the bundle installer
+cat ${CURDIR}/${SCRIPT_TEMPLATE} > ${INSTALLER}
+echo "PAYLOAD:"                 >> ${INSTALLER}
+cat ignite.tar.bz2              >> ${INSTALLER}
+cp ${INSTALLER} ${CURDIR}/.
+rm -rf ${TMPDIR}
+
